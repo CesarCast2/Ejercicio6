357b65f65606c2f8c1735de0d362d5015eb526d3
Cesar C <cc883585@gmail.com>
2024-03-08 12:25:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.ejercicio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+        val lugares = arrayOf("Amatitan", "Tequila", "Paris",
+            "Guadalaja", "Tamaulipas", "Monterrey", "Tangamandapio", "Tepito",
+            "Tulum", "Roma")
+
+        lateinit var txtTexto : TextView
+        lateinit var listView: ListView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtTexto = findViewById(R.id.txtTexto)
+        listView = findViewById(R.id.listaLugares)
+        val adaptador = ArrayAdapter(this, R.layout.list_item, lugares)
+        listView.adapter=adaptador
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+               val valor = listView.getItemAtPosition(position)
+                       as String
+                Log.e("List app", "Valor es $valor")
+                Toast.makeText(applicationContext, "Valor $valor", Toast.LENGTH_LONG).show()
+                txtTexto.text=valor
+            }
+
+        }
+    }
+
+}
